9ce0f4057bcb7d9c9c098b2a9bda4cb4c16f9240
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2015-03-25 16:55:24 +0100

    add description

--- getSeqBlat.py
+++ getSeqBlat.py
@@ -7,10 +7,17 @@
 # date: 25-03-2015
 #
 
+# Extraire des séquences à partir du résultat blat (query=rétro,mira,...; database=génome du tabac):
+# Fichiers inputs= génome au format fasta; output du blat modifié en txt (Ex: "BLAT_BX_contigs_Tnt1.txt")
+# Nom de la seq à extraire=dans la colonne "Tname"; le nom de la séq extraite=dans la colonne "Qname". 
+# Extraire la seq: 
+# Si "strand +" => "Nbr bp à extraire" upstream à partir de la position Tstart et 4000pb downstream. 
+# Si "strand -" => "Nbr pb à extraire" downstream à partir de la position Tend et 4000pb upstream en reverse complémentaire
+
 import argparse
 
 ## parse arguments
-parser = argparse.ArgumentParser()
+parser = argparse.ArgumentParser(description="Extraire les séquences à partir d'un résultat blat")
 
 parser.add_argument("modblat", help="modified blat alignment file in psl-like format: cf. QT")
 
