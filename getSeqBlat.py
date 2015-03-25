0ca60dcc51f6219176ae86df544346d06ebbf054
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2015-03-25 16:22:02 +0100

    add target fragment size optional argument

--- getSeqBlat.py
+++ getSeqBlat.py
@@ -11,8 +11,12 @@ import argparse
 
 ## parse arguments
 parser = argparse.ArgumentParser()
-parser.add_argument("modblat", help="modified blat alignment file in psl-like format: cf. QT")
-args = parser.parse_args()
-print args.modblat
 
+parser.add_argument("modblat", help="modified blat alignment file in psl-like format: cf. QT")
+
+parser.add_argument("-t", "--target_fragment_size", help="the target fragment size to extract")
+
+args = parser.parse_args()
+
+print args.target_fragment_size
 
