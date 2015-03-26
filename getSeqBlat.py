9bfd106e7ccdb39dd66cff6bc5c8db13f7f1e3ac
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2015-03-26 15:07:10 +0100

    inverse positional arguments

--- getSeqBlat.py
+++ getSeqBlat.py
@@ -12,8 +12,8 @@ import argparse
 ## parse arguments
 parser = argparse.ArgumentParser(description="Extract genomic sequences from a blat result")
 
-parser.add_argument("modblat", help="modified blat alignment file in psl-like format: cf. QT")
 parser.add_argument("genome", help="genome in fasta format")
+parser.add_argument("modblat", help="modified blat alignment file in psl-like format: cf. QT")
 
 parser.add_argument("-t", "--target_transcript_fragment_size", dest="tt_frag_sz", type=int, default=4000, help="the target transcript fragment size to extract [default: %(default)s]")
 
