9e3c063636bf60b5fbd0374edbbf981eaf1999a9
Joseph Tran <Joseph.Tran@versailles.inra.fr>
2015-03-30 15:30:32 +0200

    add logging and main section

--- getSeqBlat.py
+++ getSeqBlat.py
@@ -8,8 +8,9 @@
 #
 
 import argparse
+import logging
 
-## parse arguments
+## arguments ##
 parser = argparse.ArgumentParser(description="Extract genomic sequences from a blat result")
 
 parser.add_argument("genome", help="genome in fasta format")
@@ -19,13 +20,50 @@ parser.add_argument("-t", "--target_transcript_fragment_size", dest="tt_frag_sz"
 
 args = parser.parse_args()
 
-## load genome
-## load blat
-## iterate over blat hits and extract genomic sequences regarding the given fragment sizes (transcript and LTR) at the reference position
-### 2 cases: strand + or -
-## export fasta output
-### seq_id: Qname ; Tname ; LTR size; transcript size ; total size
-### seq
+## logging ##
+# create logger
+logger = logging.getLogger('getSeqBlat')
+logger.setLevel(logging.DEBUG)
 
-## load genome
-fasta = args.genome
+# create console handler and set level to debug
+ch = logging.StreamHandler()
+ch.setLevel(logging.DEBUG)
+
+# create formatter
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+
+# add formatter to ch
+ch.setFormatter(formatter)
+
+# add ch to logger
+logger.addHandler(ch)
+
+
+## functions ##
+
+
+### MAIN ###
+if __name__ == '__main__':
+    if __package__ is None:
+        import sys
+        from os import path
+        sys.path.append( path.dirname( path.dirname( path.abspath(__file__) ) ) )
+
+    from getSeqBlatLib import ModBlatHit
+
+    ### steps ###
+    ## load genome
+    ## load mod blat
+    ## iterate over blat hits and extract genomic sequences regarding the given fragment sizes (transcript and LTR) at the reference position
+    ### 2 cases: strand + or -
+    ## export fasta output
+    ### seq_id: Qname ; Tname ; LTR size; transcript size ; total size
+    ### seq
+
+    ## load genome
+    fasta = args.genome
+    logger.debug(fasta)
+
+    ## load mod blat
+    logger.debug(args.modblat)
+
